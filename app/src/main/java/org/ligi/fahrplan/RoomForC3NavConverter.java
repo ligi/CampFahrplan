84d3a3fa520a1e4a317bc26ebd222695fc5dc070
ligi <ligi@ligi.de>
2016-12-23 00:57:15 +0100

    Fix NPE

--- app/src/main/java/org/ligi/fahrplan/RoomForC3NavConverter.java
+++ app/src/main/java/org/ligi/fahrplan/RoomForC3NavConverter.java
@@ -2,7 +2,6 @@ package org.ligi.fahrplan;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,9 +21,9 @@ public class RoomForC3NavConverter {
     }};
 
     @Nullable
-    public static String convert(@NonNull final String venue, @NonNull final String room) {
-        if (venue.toUpperCase().equals("CCH")) {
-            return CCH_MAP.get(room.toUpperCase().replace("SAAL","HALL"));
+    public static String convert(@NonNull final String venue, final String room) {
+        if (venue.toUpperCase().equals("CCH") && room != null) {
+            return CCH_MAP.get(room.toUpperCase().replace("SAAL", "HALL"));
         }
         return null;
     }

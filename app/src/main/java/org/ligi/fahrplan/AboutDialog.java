a15963ee6592710ee938b12c43175fea7b4a88b0
ligi <ligi@ligi.de>
2016-12-25 21:41:57 +0100

    Add and use HtmlCompat

--- app/src/main/java/org/ligi/fahrplan/AboutDialog.java
+++ app/src/main/java/org/ligi/fahrplan/AboutDialog.java
@@ -4,12 +4,12 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.content.ContextCompat;
-import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import org.ligi.compat.HtmlCompat;
 import org.ligi.fahrplan.congress.BuildConfig;
 import org.ligi.fahrplan.congress.R;
 
@@ -43,27 +43,27 @@ public class AboutDialog extends DialogFragment {
         Context context = getContext();
 
         TextView logo_copyright = (TextView) view.findViewById(R.id.copyright_logo);
-        logo_copyright.setText(Html.fromHtml(getString(R.string.copyright_logo)));
+        logo_copyright.setText(HtmlCompat.fromHtml(getString(R.string.copyright_logo)));
         logo_copyright.setLinkTextColor(ContextCompat.getColor(context, R.color.text_link_color_dark));
         logo_copyright.setMovementMethod(LinkMovementMethod.getInstance());
 
         TextView conferenceUrl = (TextView) view.findViewById(R.id.conference_url);
-        conferenceUrl.setText(Html.fromHtml(getString(R.string.conference_url)));
+        conferenceUrl.setText(HtmlCompat.fromHtml(getString(R.string.conference_url)));
         conferenceUrl.setMovementMethod(LinkMovementMethod.getInstance());
         conferenceUrl.setLinkTextColor(ContextCompat.getColor(context, R.color.text_link_color_dark));
 
         TextView sourceCode = (TextView) view.findViewById(R.id.source_code);
-        sourceCode.setText(Html.fromHtml(getString(R.string.source_code)));
+        sourceCode.setText(HtmlCompat.fromHtml(getString(R.string.source_code)));
         sourceCode.setMovementMethod(LinkMovementMethod.getInstance());
         sourceCode.setLinkTextColor(ContextCompat.getColor(context, R.color.text_link_color_dark));
 
         TextView issues = (TextView) view.findViewById(R.id.issues);
-        issues.setText(Html.fromHtml(getString(R.string.issues)));
+        issues.setText(HtmlCompat.fromHtml(getString(R.string.issues)));
         issues.setMovementMethod(LinkMovementMethod.getInstance());
         issues.setLinkTextColor(ContextCompat.getColor(context, R.color.text_link_color_dark));
 
         TextView googlePlayStore = (TextView) view.findViewById(R.id.google_play_store);
-        googlePlayStore.setText(Html.fromHtml(getString(R.string.google_play_store)));
+        googlePlayStore.setText(HtmlCompat.fromHtml(getString(R.string.google_play_store)));
         googlePlayStore.setMovementMethod(LinkMovementMethod.getInstance());
         googlePlayStore.setLinkTextColor(ContextCompat.getColor(context, R.color.text_link_color_dark));
 

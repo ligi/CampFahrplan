ecaafbc95f289e33b8531b1a46258a49400fe812
ligi <ligi@ligi.de>
2016-12-25 11:44:24 +0100

    Add some halls and adapt to new style for h1 , h2

--- app/src/main/java/org/ligi/fahrplan/RoomForC3NavConverter.kt
+++ app/src/main/java/org/ligi/fahrplan/RoomForC3NavConverter.kt
@@ -3,14 +3,17 @@ package org.ligi.fahrplan
 object RoomForC3NavConverter {
 
     private val CCH_MAP = mapOf(
-            "HALL 1" to "h1",
-            "HALL 2" to "h2",
+            "HALL 1" to "g:h1",
+            "HALL 2" to "g:h2",
             "HALL 3" to "h3",
             "HALL 6" to "h6",
             "HALL 13" to "h13",
             "HALL 14" to "h14",
 
             "HALL B" to "hb",
+            "HALL C" to "hc",
+            "HALL D" to "hd",
+            "HALL E" to "he",
             "HALL F" to "hf",
             "HALL G" to "hg",
             "HALL H" to "hh"
